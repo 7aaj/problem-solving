5a47a47bed4f9016cb9e784081ac6cd1cdf0166f
7aaj <101370326+7aaj@users.noreply.github.com>
2022-06-28 14:40:30 +0300

    Update main.js

--- HackerRank/mini-max Sum/main.js
+++ HackerRank/mini-max Sum/main.js
@@ -1,11 +1,11 @@
 function miniMaxSum(arr) {
-  let arrSum = 0;
+  let arrayResult = 0;
   // loop to have the sum of the array numbers
   for (let i = 0; i < arr.length; i++) {
     // saving the array sum in a variable
-    arrSum += arr[i];
+    arrayResult += arr[i];
   }
-  console.log(`${arrSum - Math.max(...arr)} ${arrSum - Math.min(...arr)}`);
+  console.log(`${arrayResult - Math.max(...arr)} ${arrayResult - Math.min(...arr)}`);
 }
 
 miniMaxSum([1, 2, 3, 4, 5]);
